05c02ad3ab0635fc016cd86e0b308c5c5730f8c7
Tomas704 <150282656+Tomas704@users.noreply.github.com>
2025-10-21 00:37:53 +0200

    Update interpreter.cpp

--- turtlepreter/interpreter.cpp
+++ turtlepreter/interpreter.cpp
@@ -3,39 +3,140 @@
 
 #include "heap_monitor.hpp"
 
-namespace turtlepreter {
+namespace turtlepreter
+{
 
-// TODO Node definitions
+    Node::Node() : m_parent(nullptr), m_command(nullptr)
+    {
+    }
 
-// ==================================================
+    Node::Node(ICommand *command) : m_parent(nullptr), m_command(command)
+    {
+    }
 
-// TODO CommandMove definitions
+    Node::~Node()
+    {
+        for (Node *subnode : m_subnodes)
+        {
+            delete subnode;
+        }
+        delete m_command;
+    }
 
-// ==================================================
+    std::string Node::toString() const
+    {
+        if (m_command == nullptr)
+        {
+            return "No command";
+        }
+        return m_command->toString();
+    }
 
-// TODO CommandJump definitions
+    void Node::addSubnode(Node *subnode)
+    {
+        if (subnode != nullptr)
+        {
+            m_subnodes.push_back(subnode);
+            subnode->m_parent = this;
+        }
+    }
 
-// ==================================================
+    Node *Node::getParent() const
+    {
+        return m_parent;
+    }
 
-// TODO CommandRotate definitions
+    const std::vector<Node *> &Node::getSubnodes() const
+    {
+        return m_subnodes;
+    }
 
-// ==================================================
+    ICommand *Node::getCommand() const
+    {
+        return m_command;
+    }
 
-Interpreter::Interpreter(Node *root) : m_root(root) {
-}
+    // ==================================================
 
-void Interpreter::interpretAll(Turtle &turtle) {
-    this->interpterSubtreeNodes(m_root, turtle);
-}
+    // TODO CommandMove definitions
 
-Node *Interpreter::getRoot() const {
-    return m_root;
-}
+    CommandMove::CommandMove(float d) : m_d(d)
+    {
+        //
+    }
 
-void Interpreter::interpterSubtreeNodes(Node *node, Turtle &turtle) {
-    (void)node;
-    (void)turtle;
-    throw std::logic_error("Not implemented yet!");
-}
+    void CommandMove::execute(Turtle &turtle)
+    {
+        turtle.move(m_d); //
+    }
+
+    std::string CommandMove::toString()
+    {
+        //
+        return "Move(" + std::to_string(m_d) + ")";
+    }
+
+    // ==================================================
+
+    // TODO CommandJump definitions
+
+    CommandJump::CommandJump(float x, float y) : m_x(x), m_y(y)
+    {
+        //
+    }
+
+    void CommandJump::execute(Turtle &turtle)
+    {
+        turtle.jump(m_x, m_y); //
+    }
+
+    std::string CommandJump::toString()
+    {
+        //
+        return "Jump(" + std::to_string(m_x) + ", " + std::to_string(m_y) + ")";
+    }
+
+    // ==================================================
+
+    // TODO CommandRotate definitions
+
+    CommandRotate::CommandRotate(float angleRad) : m_angleRad(angleRad)
+    {
+        //
+    }
+
+    void CommandRotate::execute(Turtle &turtle)
+    {
+        turtle.rotate(m_angleRad); //
+    }
+
+    std::string CommandRotate::toString()
+    {
+        //
+        return "Rotate(" + std::to_string(m_angleRad) + ")";
+    }
+
+    // ==================================================
+
+    Interpreter::Interpreter(Node *root) : m_root(root)
+    {
+    }
+
+    void Interpreter::interpretAll(Turtle &turtle)
+    {
+        this->interpterSubtreeNodes(m_root, turtle);
+    }
+
+    Node *Interpreter::getRoot() const
+    {
+        return m_root;
+    }
+
+    void Interpreter::interpterSubtreeNodes(Node *node, Turtle &turtle)
+    {
+        (void)node;
+        (void)turtle;
+        throw std::logic_error("Not implemented yet!");
+    }
 
 } // namespace turtlepreter
